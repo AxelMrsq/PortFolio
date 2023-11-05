3fedb5cbfb78c47e0f1f09c68e0bcf42785ec867
Marsattak38 <110765175+Marsattak38@users.noreply.github.com>
2023-11-05 08:37:10 +0100

    05/11/2023
    Section Part1 + JS

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Sélectionnez les éléments que vous souhaitez animer
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+  
+    // Ajoutez une classe CSS pour l'animation
+    header.classList.add("fade-in");
+    main.classList.add("fade-in");
+  });
+ 
+  
