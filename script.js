3ffa602c3498019d72a514c29f2c1f97aecbe1db
Marsattak38 <110765175+Marsattak38@users.noreply.github.com>
2023-11-12 20:35:13 +0100

    13/11/2023
    Start project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Sélectionnez les éléments que vous souhaitez animer
-    const header = document.querySelector("header");
-    const main = document.querySelector("main");
+    const header = document.querySelector(".presentation");
+    
   
     // Ajoutez une classe CSS pour l'animation
     header.classList.add("fade-in");
